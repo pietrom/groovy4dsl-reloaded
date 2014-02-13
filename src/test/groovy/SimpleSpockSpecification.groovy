e1d115707a64f0b5b6ce7d4bb2a3d46367c4e83f
Pietro Martinelli <amicofragile@gmail.com>
2014-02-13 22:52:08 +0100

    Spock given-when-then example

--- src/test/groovy/SimpleSpockSpecification.groovy
+++ src/test/groovy/SimpleSpockSpecification.groovy
@@ -1,11 +1,23 @@
 import spock.lang.Specification
 
 class SimpleSpockSpecification extends Specification {
-    def "the truth could not be truer" () {
-        given: 
-            def truth = true
+	def "the truth could not be truer" () {
+        	given: 
+	            def truth = true
 
-        expect:
-            truth
-    }
+        	expect:
+	            truth
+	}
+
+	void "two wrongs don't make a right" () {
+	    given: "two false statements"
+        	def theWorldIsFlat = false
+	        def theEarthOrbitsTheSun = false
+
+	    when: "we combine the two falsehoods"
+        	def copernicusWasWrong = theWorldIsFlat && theEarthOrbitsTheSun
+		
+	    then: "Copernicus was telling the truth"
+        	! copernicusWasWrong
+	}
 }
