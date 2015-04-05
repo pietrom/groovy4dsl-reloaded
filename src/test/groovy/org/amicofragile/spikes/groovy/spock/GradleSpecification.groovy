06a9f7b045918e1ac84b48ef83962c4f089437d1
Pietro Martinelli <amicofragile@gmail.com>
2015-04-05 18:21:35 +0200

    Fixed GradleSpecification to take OS into account (for line separator test in output and for gradle command name)

--- src/test/groovy/org/amicofragile/spikes/groovy/spock/GradleSpecification.groovy
+++ src/test/groovy/org/amicofragile/spikes/groovy/spock/GradleSpecification.groovy
@@ -5,11 +5,28 @@ import spock.lang.Specification
 class GradleSpecification extends Specification {
 	def "hello task says Hello, World!"() {
 		given: "we have a gradle build command"
-			def command = "gradle -q -b gradle/hello-with-actions.gradle hello"
+			def command = "${getGradleCommand()} -q -b gradle/hello-with-actions.gradle hello"
 		when: "we run the build command"
 			def proc = command.execute()
 			proc.waitFor()
 		then: "the script outputs the correct details"
-			"And this one?\nFirst\nLast" == proc.in.text.trim()
+			def result = proc.in.text.trim().replaceAll(System.getProperty("line.separator"), "\n")
+			"And this one?\nFirst\nLast" == result
+	}
+	
+	private String getOperatingSystem() {
+		System.getProperty("os.name").toLowerCase()
+	}
+	
+	private boolean isWindows() {
+		getOperatingSystem().indexOf("win") >= 0
+	}
+	
+	private String getGradleCommand() {
+		String cmd = "gradle"
+		if(isWindows()) {
+			cmd += ".bat"
+		}
+		return cmd
 	}
 }
